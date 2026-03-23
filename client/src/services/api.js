b4deab3642e6dd451cd7e1c09e852b3fcbd6eb1a
kartsinghal <kartiksinghal844@gmail.com>
2026-03-23 11:34:42 +0530

    Change API base URL to production environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'mini-project-production-ee75.up.railway.app',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
